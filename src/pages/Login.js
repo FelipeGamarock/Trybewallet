983b4212b44c8d60ec9050573903f9e8c5dabde6
Felipe Gama Danta <epilefgama2@gmail.com>
2022-03-31 17:40:58 -0300

    Paginas Login e Carteira criadas, req1 66%, req2 feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisable: false,
+      redirect: false,
+    };
+  }
+
+  handleInput = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  effectLogin = () => {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisable, redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/carteira" />;
+    }
+
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          name="email"
+          type="text"
+          value={ email }
+          onChange={ this.handleInput }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          type="password"
+          value={ password }
+          onChange={ this.handleInput }
+        />
+        <button
+          type="button"
+          onClick={ this.effectLogin }
+          disabled={ isDisable }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
